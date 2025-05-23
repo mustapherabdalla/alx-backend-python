687cbfda50b0fe3701047cdee170c43e07b15c4a
Mustapher Abdalla <mustapherabdalla@gmail.com>
2025-05-23 20:37:41 +0300

    Update 4-stream_ages.py

--- python-generators-0x00/4-stream_ages.py
+++ python-generators-0x00/4-stream_ages.py
@@ -1,1 +1,21 @@
+import mysql.connector
 
+def stream_user_ages():
+    connection = mysql.connector.connect(host="localhost", user="root", passwd="", database="ALX_prodev")
+    cursor = connection.cursor()
+    query = "SELECT age FROM user_data WHERE 1"
+    cursor.execute(query)
+    ages = cursor.fetchall()
+    for (age,) in ages:
+        yield age
+    return None
+
+
+def calculate_average():
+    total = 0
+    ages = stream_user_ages()
+    for age in ages:
+        total = total + age
+    print("Average age of users: ", total)
+
+calculate_average()
