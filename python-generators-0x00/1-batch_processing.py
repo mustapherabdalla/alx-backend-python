20df958b4cb751cff68ccc6969dc37392ba89ae6
Mustapher Abdalla <mustapherabdalla@gmail.com>
2025-05-23 18:01:01 +0300

    Update 1-batch_processing.py

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -1,1 +1,26 @@
+import mysql.connector
+
+def stream_users_in_batches(batch_size):
+    connection = mysql.connector.connect(host="localhost", user="root", passwd="", database="ALX_prodev")
+    cursor = connection.cursor(dictionary=True)
+    query = "SELECT * FROM user_data"
+    cursor.execute(query)
+    rows = cursor.fetchall()
+
+    for i in range(0, len(rows), batch_size):
+        batches = rows[i: i + batch_size]
+        results = [batch for batch in batches]
+        yield results
+
+
+def batch_processing(batch_size):
+    batches = stream_users_in_batches(batch_size)
+
+    for batch in batches:
+        for i in range(0, len(batch)):
+            user = batch[i]
+            if user['age'] < 25:
+                continue
+            print(user)
+        print("\n\n")
 
