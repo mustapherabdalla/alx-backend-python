c073547edb30d4c62cad8ae94606facd9049e5b9
Mustapher Abdalla <mustapherabdalla@gmail.com>
2025-05-23 12:17:19 +0300

    Update 0-stream_users.py

--- python-generators-0x00/0-stream_users.py
+++ python-generators-0x00/0-stream_users.py
@@ -1,1 +1,13 @@
+import mysql.connector
+
+
+def stream_users():
+    connection = mysql.connector.connect(host="localhost", user="root", passwd="", database="ALX_prodev")
+    cursor = connection.cursor(dictionary=True)
+    query = "SELECT * FROM user_data"
+    cursor.execute(query)
+    rows = cursor.fetchall()
+
+    for row in rows:
+        yield row
 
