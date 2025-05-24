345012e688317e1e9ed3def1988dddbd889eb3f5
Mustapher Abdalla <mustapherabdalla@gmail.com>
2025-05-24 10:41:21 +0300

    Update 2-lazy_paginate.py

--- python-generators-0x00/2-lazy_paginate.py
+++ python-generators-0x00/2-lazy_paginate.py
@@ -1,1 +1,19 @@
+import mysql.connector
 
+
+def paginate_users(page_size, offset):
+    connection = mysql.connector.connect(host="localhost", user="root", passwd="", database="ALX_prodev")
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute(f"SELECT * FROM user_data LIMIT {page_size} OFFSET {offset}")
+    rows = cursor.fetchall()
+    return rows
+
+
+def lazy_paginate(page_size):
+    offset = 0
+    current_page = paginate_users(page_size, offset)
+
+    for i in range(len(current_page)):
+        if not current_page:
+                break
+        yield current_page
