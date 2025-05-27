1a2b9233e9fd73055003da9a2e17194c0307f193
Mustapher Abdalla <mustapherabdalla@gmail.com>
2025-05-27 11:26:38 +0300

    Update 1-with_db_connection.py

--- python-decorators-0x01/1-with_db_connection.py
+++ python-decorators-0x01/1-with_db_connection.py
@@ -6,7 +6,14 @@ def with_db_connection(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         connection = mysql.connector.connect(host="localhost", user="root", passwd="", database="ALX_prodev")
-        return func(connection, *args, **kwargs)
+        try:
+            return func(connection, *args, **kwargs)
+        except Exception as e:
+            print(f"Database error: {e}")
+            raise
+        finally:
+            connection.close()
+
     return wrapper
 
 @with_db_connection
@@ -14,6 +21,7 @@ def get_user_by_id(connection, user_id):
     cursor = connection.cursor()
     cursor.execute(f"SELECT * FROM user_data WHERE user_id = {user_id}")
     return cursor.fetchone()
+
 
 #### Fetch user by ID with automatic connection handling
 user = get_user_by_id(user_id=1)
