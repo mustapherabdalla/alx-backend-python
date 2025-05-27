bb53561a624dcd9d1def0dabcb9c7a12a45e42f9
Mustapher Abdalla <mustapherabdalla@gmail.com>
2025-05-27 10:34:55 +0300

    Update 0-log_queries.py

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,1 +1,21 @@
+import mysql.connector
 
+def log_queries():
+    def decorator(func):
+        def wrapper(query, *args, **kwargs):
+            print(query)
+            return func(query, *args, **kwargs)
+        return wrapper
+    return decorator
+
+@log_queries()
+def fetch_all_users(query):
+    connection = mysql.connector.connect(host="localhost", user="root", passwd="", database="ALX_prodev")
+    cursor = connection.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    connection.close()
+    return results
+
+#### fetch users while logging the query
+users = fetch_all_users(query="SELECT * FROM user_data")
