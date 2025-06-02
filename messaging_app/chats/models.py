d498fd0a9edfcd24f04a024a4351b7fd3407ee65
Mustapher Abdalla <mustapherabdalla@gmail.com>
2025-06-02 12:49:08 +0300

    Update models.py

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,23 +1,26 @@
+import uuid
 from django.db import models # type: ignore
 from django.contrib.auth.models import AbstractUser # type: ignore
+from django.utils.translation import gettext_lazy as _ # type: ignore
 
-# Create your models here.
 class User(AbstractUser):
-  phone_number = models.CharField(max_length=100)
-  role = models.CharField(max_length=100)
-  
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    email = models.EmailField(max_length=200, unique=True, blank=False, null=False)
+    first_name = models.CharField(max_length=150, null=False, blank=False)
+    last_name = models.CharField(max_length=150, null=False, blank=False)
+
+
 class Conversation(models.Model):
-  participants = models.ManyToManyField(User, related_name='conversations')
-  created_at = models.DateTimeField(auto_now_add=True)
-  updated_at = models.DateTimeField(auto_now=True)
-  is_group = models.BooleanField(default=False)
-  name = models.CharField(max_length=200, null=True, blank=True)
+    conversation_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    participants = models.ManyToManyField(User, related_name='conversations')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
 
 class Message(models.Model):
-  conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
-  sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
-  receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recived_messages')
-  content = models.TimeField()
-  sent_at = models.DateTimeField(auto_now_add=True)
-  read = models.BooleanField(default=False)
-  
+    message_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
+    message_body = models.TextField()
+    sent_at = models.DateTimeField(auto_now_add=True)
