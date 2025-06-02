866f1e4ad850ec7b7b20ffcb158c2baf20a210b6
Mustapher Abdalla <mustapherabdalla@gmail.com>
2025-06-02 21:26:21 +0300

    Update views.py

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render # type: ignore
+from rest_framework import generics # type: ignore
+from .models import *
+from .serializers import *
+
+# Create your views here.
+class ConversationListCreateAPIView(generics.ListCreateAPIView):
+  queryset = Conversation.objects.all()
+  serializer_class = ConversationSerializer
+
+
+class MessageListCreateAPIView(generics.ListCreateAPIView):
+  queryset = Message.objects.all()
+  serializer_class = MessageSerializer
 
 # Create your views here.
